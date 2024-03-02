33e9b9190154034de93f1bf293a3b3cd561c5f61
0x7o <danil.kononyuk@ya.ru>
2024-03-02 19:32:59 +0800

    Support dtype for training

--- ae/model.py
+++ ae/model.py
@@ -3,35 +3,38 @@ import flax.linen as nn
 from einops import rearrange, repeat
 
 
-def create_mask(seq_len):
-    mask = jnp.tril(jnp.ones((seq_len, seq_len)))
+def create_mask(seq_len, dtype: jnp.dtype = jnp.float32):
+    mask = jnp.tril(jnp.ones((seq_len, seq_len), dtype=dtype))
     return mask
 
 
-def fixed_pos_embedding(inv_freq, seq):
-    sinusoid_inp = jnp.einsum("i , j -> i j", jnp.arange(seq), inv_freq)
+def fixed_pos_embedding(inv_freq, seq, dtype: jnp.dtype = jnp.float32):
+    sinusoid_inp = jnp.einsum("i , j -> i j", jnp.arange(seq, dtype=dtype), inv_freq)
     sinusoid_inp = repeat(sinusoid_inp, "... d -> ... (d r)", r=2)
     return jnp.sin(sinusoid_inp), jnp.cos(sinusoid_inp)
 
 
-def rotate_every_two(x):
+def rotate_every_two(x, dtype: jnp.dtype = jnp.float32):
     x = rearrange(x, "... (d r) -> ... d r", r=2)
     x1, x2 = x[..., 0], x[..., 1]
-    x = jnp.stack((-x2, x1), axis=-1)
+    x = jnp.stack((-x2, x1), axis=-1, dtype=dtype)
     return rearrange(x, "... d r -> ... (d r)")
 
 
-def apply_rotary_pos_emb(x, sincos):
+def apply_rotary_pos_emb(x, sincos, dtype: jnp.dtype = jnp.float32):
     sin, cos = sincos
-    return (x * cos) + (rotate_every_two(x) * sin)
+    return (x * cos) + (rotate_every_two(x, dtype) * sin)
 
 
 class LayerNorm(nn.Module):
     eps: float = 1e-5
+    dtype: jnp.dtype = jnp.float32
 
     def __call__(self, x):
-        mean = jnp.mean(x, axis=-1, keepdims=True)
-        mean_of_squares = jnp.mean(jnp.square(x), axis=-1, keepdims=True)
+        mean = jnp.mean(x, axis=-1, keepdims=True, dtype=self.dtype)
+        mean_of_squares = jnp.mean(
+            jnp.square(x), axis=-1, keepdims=True, dtype=self.dtype
+        )
         variance = mean_of_squares - jnp.square(mean)
         inv = 1.0 / jnp.sqrt(variance + self.eps)
         return inv * (x - mean)
@@ -52,25 +55,29 @@ class SelfAttention(nn.Module):
     n_heads: int
     d_model: int
     seq_len: int
+    dtype: jnp.dtype = jnp.float32
 
     def setup(self) -> None:
         self.heads = [SelfAttentionHead() for _ in range(self.n_heads)]
-        self.out = nn.Dense(self.d_model)
+        self.out = nn.Dense(self.d_model, dtype=self.dtype)
 
     def __call__(self, v, k, q, wi, mask=None):
         outputs = [head(v, k, q, wi, mask) for head in self.heads]
-        return self.out(jnp.concatenate(outputs, axis=-1))
+        return self.out(jnp.concatenate(outputs, axis=-1, dtype=self.dtype))
 
 
 class PositionEncoding(nn.Module):
     d_model: int
+    dtype: jnp.dtype = jnp.float32
 
     @nn.compact
     def __call__(self, x):
         seq_len = x.shape[1]
-        inv_freq = 1.0 / (10000 ** (jnp.arange(0, self.d_model, 2) / self.d_model))
-        pos_enc = fixed_pos_embedding(inv_freq, seq_len)
-        return apply_rotary_pos_emb(x, pos_enc)
+        inv_freq = 1.0 / (
+            10000 ** (jnp.arange(0, self.d_model, 2, dtype=self.dtype) / self.d_model)
+        )
+        pos_enc = fixed_pos_embedding(inv_freq, seq_len, dtype=self.dtype)
+        return apply_rotary_pos_emb(x, pos_enc, dtype=self.dtype)
 
 
 class FeedForward(nn.Module):
@@ -84,12 +91,16 @@ class Block(nn.Module):
     d_model: int
     n_heads: int
     seq_len: int
+    dtype: jnp.dtype = jnp.float32
 
     def setup(self) -> None:
         self.attention = SelfAttention(
-            n_heads=self.n_heads, d_model=self.d_model, seq_len=self.seq_len
+            n_heads=self.n_heads,
+            d_model=self.d_model,
+            seq_len=self.seq_len,
+            dtype=self.dtype,
         )
-        self.norm = LayerNorm()
+        self.norm = LayerNorm(dtype=self.dtype)
         self.wi = self.param(
             "wi", nn.initializers.xavier_uniform(), (self.d_model, self.d_model)
         )
@@ -108,22 +119,24 @@ class LM(nn.Module):
     n_layers: int
     vocab_size: int
     seq_len: int
+    dtype: jnp.dtype = jnp.float32
 
     def setup(self) -> None:
-        self.w_e = nn.Embed(self.vocab_size, self.d_model)
-        self.p_e = PositionEncoding(d_model=self.d_model)
+        self.w_e = nn.Embed(self.vocab_size, self.d_model, dtype=self.dtype)
+        self.p_e = PositionEncoding(d_model=self.d_model, dtype=self.dtype)
         self.blocks = [
             Block(
                 d_model=self.d_model,
                 n_heads=self.n_heads,
                 seq_len=self.seq_len,
+                dtype=self.dtype,
             )
             for _ in range(self.n_layers)
         ]
-        self.out = nn.Dense(self.vocab_size)
+        self.out = nn.Dense(self.vocab_size, dtype=self.dtype)
 
     def __call__(self, x):
-        mask = create_mask(x.shape[-1])
+        mask = create_mask(x.shape[-1], dtype=self.dtype)
         x = self.w_e(x)
         x = self.p_e(x)
         for block in self.blocks:
